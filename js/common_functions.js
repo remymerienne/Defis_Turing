fbb1c76f41678b2f07a9b847942a4a3755e3b263
Rémy Merienne <remy.merienne-dev@outlook.fr>
2022-07-14 18:27:29 +0700

    refactore

--- js/common_functions.js
+++ js/common_functions.js
@@ -3,10 +3,3 @@ export const getSumOfArrayNumbers = (array) => {
     (accumulateur, valeurCourante) => accumulateur + valeurCourante
   );
 };
-
-export const displaySolution = (number, solution) => {
-  const start = performance.now();
-  console.log('Défis N' + number + ' : ' + solution);
-  const end = performance.now();
-  console.log(end - start + ' ms.');
-};
--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
-import { displaySolution } from './common_functions.js';
 import { turing_1Solution } from './turing/turing_1.js';
 import { turing_2Solution } from './turing/turing_2.js';
 
+const displaySolution = (number, solution) => {
+  console.log('Défis N' + number + ' : ' + solution);
+};
+
+const start = performance.now();
+
 displaySolution(1, turing_1Solution());
 displaySolution(2, turing_2Solution());
+
+const end = performance.now();
+console.log(end - start + ' ms.');
