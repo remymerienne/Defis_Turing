4689f14248162ab9caedf653b72d0bca03d88de1
Rémy Merienne <remy.merienne-dev@outlook.fr>
2022-08-10 16:02:39 +0200

    [REFACTOR]: clean function

--- js/arithmetic/fibonacci_sequence.js
+++ js/arithmetic/fibonacci_sequence.js
@@ -1,32 +1,16 @@
-/**
- * @module fibonacci_sequence
- * @description Fonction permettant de générer la Suite de Fibonacci.
- * @author Rémy Merienne
- */
-
-/**
- * @function
- * @name getFibonacciSequence
- * @description Retourne la suite de Fibonacci comprise entre 0 et le nombre passé en argument.
- * @param {number} maximumValue - Nombre le plus grand pouvant figurer dans la liste
- * @returns {number[]}
- */
 const getFibonacciSequence = (maximumValue) => {
-  // Debut de la suite par convention.
-  let sequence = [0, 1];
-  let l = sequence.length;
-  // Chaque nouvel élément sera constitué de la somme des deux éléments qui le précède.
-  let nextItem = sequence[l - 2] + sequence[l - 1];
-  // Tant que la valeur maximum n'est pas atteinte :
+  const fibonacciSequence = [0, 1];
+
+  let length = fibonacciSequence.length;
+  let nextItem = fibonacciSequence[length - 2] + fibonacciSequence[length - 1];
+
   while (nextItem <= maximumValue) {
-    // On ajoute le nouvel élément créé.
-    sequence.push(nextItem);
-    // La longueur de  la suite est recalculée.
-    l = sequence.length;
-    // On génère un nouvel élément.
-    nextItem = sequence[l - 2] + sequence[l - 1];
+    fibonacciSequence.push(nextItem);
+    length = fibonacciSequence.length;
+    nextItem = fibonacciSequence[length - 2] + fibonacciSequence[length - 1];
   }
-  return sequence;
+
+  return fibonacciSequence;
 };
 
 export { getFibonacciSequence };
